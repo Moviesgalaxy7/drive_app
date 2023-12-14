0346a1bf26d735d9dfebff15cae346fb3f6ba9f0
MG <themoviesgalaxy@gmail.com>
2023-12-15 00:29:03 +0530

    db

--- config.php
+++ config.php
@@ -4,14 +4,25 @@
 								GLOBLE VALUES
 ===================================================================================== */
 
+define('IS_PRODUCTION', ($_SERVER['REMOTE_ADDR'] === '103.250.161.134') ? true : false);
+
 /*
 =====================================================================================
 								DATABASE VALUES
 ===================================================================================== */
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'mgdrive');
+
+if (IS_PRODUCTION) {
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'moviesga_mg_admin');
+	define('DB_PASS', 'D@9s:EX3n;keM`&/x4=)');
+	define('DB_NAME', 'moviesga_mgdrive');
+} else {
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', '');
+	define('DB_NAME', 'mgdrive');
+}
+
 
 /*
 =====================================================================================
---
+++ db/db.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Dec 14, 2023 at 08:56 PM
+-- Server version: 8.0.35
+-- PHP Version: 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `moviesga_mgdrive`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `token`
+--
+
+CREATE TABLE `token` (
+  `id` int NOT NULL,
+  `token` text COLLATE utf8mb3_unicode_ci,
+  `expires_in` smallint NOT NULL,
+  `scope` text COLLATE utf8mb3_unicode_ci NOT NULL,
+  `token_type` varchar(30) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci COMMENT='Save access token';
+
+
+--
+-- Indexes for table `token`
+--
+ALTER TABLE `token`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `token`
+--
+ALTER TABLE `token`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
